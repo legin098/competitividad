1ff865b582c999d15bd95672b57edbce9c51a5e9
Jesus Cruz <jesus.cruz@campusvirtual.aunarvillavicencio.edu.co>
2022-11-25 08:57:19 -0500

    Empresa detail

--- src/views/EmpresaDetailScreen.js
+++ src/views/EmpresaDetailScreen.js
@@ -2,52 +2,44 @@ import Map from "components/competitividad/Map";
 import credentials from "components/competitividad/credentials";
 import { Container, Row, Col } from "react-bootstrap";
 import { useSelector } from "react-redux";
+import Loader from "components/Loader";
+import Message from "components/Message";
 
 const EmpresaDetailScreen = () => {
-  const { empresaDetail } = useSelector((state) => state.empresas);
+  const { empresaDetail, loading, error } = useSelector(
+    (state) => state.empresas
+  );
 
   return (
-    <>
-      <section className="pt-5">
-        <Container className="pt-5" fluid>
-          <Row className="justify-content-center">
-            <Col className="text-center" lg="8">
-              <h1 style={{ color: "#0A81C4" }}>{empresaDetail.nombre}</h1>
-            </Col>
-          </Row>
-          <Row>
-            <Col className="p-0">
-              <Container>
-                <Map
-                  googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&key=${credentials.mapKey}`}
-                  containerElement={<div style={{ height: "500px" }} />}
-                  mapElement={<div style={{ height: "100%" }} />}
-                  loadingElement={<p>Cargando...</p>}
-                  lat={Number(empresaDetail.lat)}
-                  lon={Number(empresaDetail.lon)}
-                />
-              </Container>
-            </Col>
-          </Row>
-          <Row
-            className="justify-content-center"
-            style={{ backgroundColor: "#0A81C4" }}
-          >
-            <Col className="py-5" lg="6">
-              <Container>
-                <p className="text-center" style={{ color: "white" }}>
-                  La integración local y la generación de medios de vida,
-                  mediante el acceso a la oferta local y la formación específica
-                  requerida para el desarrollo personal y empresarial, aportando
-                  a la disminución de las brechas y barreras para el desarrollo
-                  social y económico.{" "}
-                </p>
-              </Container>
-            </Col>
-          </Row>
-        </Container>
-      </section>
-    </>
+    <div style={{ paddingTop: "7rem" }}>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <div>
+          <Container>
+            <Row>
+              <Col md={4}>
+                <Container className="p-0">
+                  <Map
+                    googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&key=${credentials.mapKey}`}
+                    containerElement={<div style={{ height: "500px" }} />}
+                    mapElement={<div style={{ height: "100%" }} />}
+                    loadingElement={<p>Cargando...</p>}
+                    lat={Number(empresaDetail.lat)}
+                    lon={Number(empresaDetail.lon)}
+                  />
+                  <h3 className="mt-3" style={{ color: "#0A81C4" }}>{empresaDetail.nombre}</h3>
+                </Container>
+              </Col>
+              <Col md={4}>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      )}
+    </div>
   );
 };
 export default EmpresaDetailScreen;
